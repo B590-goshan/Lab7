4c196a2f9e02ed5a09e6cdd74da75d94c0b364db
Gokul Kaarthick <goshan@iu.edu>
2025-02-18 18:15:22 -0500

    first commit

---
+++ app/src/main/java/com/example/lab7/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lab7
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Fix: Ensure ID exists in activity_main.xml
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Fix: Ensure fragment is displayed
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, TicketDetailFragment())
+            .commit()
+    }
+}
