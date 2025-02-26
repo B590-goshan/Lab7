b8e55c2e648257fb7935b1a14e0dcd1ee3ae24ad
subavenk <subavenk@iu.edu>
2025-02-26 17:24:13 -0500

    Homework3 Part5 - Completed

--- app/src/main/java/com/example/lab7/MainActivity.kt
+++ app/src/main/java/com/example/lab7/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.lab7
+package com.iub.lab7
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -11,17 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        // Fix: Ensure ID exists in activity_main.xml
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Fix: Ensure fragment is displayed
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, TicketListFragment())
-            .commit()
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment_container, TicketListFragment())
+//            .commit()
     }
 }
