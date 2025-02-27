15627436d78fb8f21f95435026fbf711857c7e1b
Gokul Kaarthick <goshan@iu.edu>
2025-02-27 12:01:41 -0500

    H3P7-challenge

--- app/src/main/java/com/example/lab7/database/TicketDatabase.kt
+++ app/src/main/java/com/example/lab7/database/TicketDatabase.kt
@@ -1,8 +1,12 @@
 package com.example.lab7.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.lab7.database.TicketTypeConverter
 import com.example.lab7.Ticket
 
